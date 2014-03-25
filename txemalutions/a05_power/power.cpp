2fcb1d1be7112afcb5a4bb65f821729ab5e611f4
txemagon <txema.gonz@gmail.com>
2014-03-25 10:34:34 +0100

    getopt usage

--- txemalutions/a05_power/power.cpp
+++ txemalutions/a05_power/power.cpp
@@ -1,9 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <strings.h>
+
+#define ENGLISH 0
+#define SPANISH 1
+#define MAX 0x100
+
+void print_usage(const char *command_name){
+    fprintf(stderr, 
+	    "Usage: %s [options] <base> <exponent>\n"
+	    "\n"
+	    "\n This program raises base to exponent." 
+	    "\n"
+	    "\n\t <base>\t\t is a real number."
+	    "\n"
+	    "\n\t <exponent>\t is an integer."
+	    "\n"
+	    "\nOPTIONS"
+	    "\n======="
+	    "\n"
+	    "\n\t -h\t\t Shows help"
+	    "\n",
+	    command_name);
+}
 
 double power(double base, int exponent){
 
     double result = 1;
+    bool   help_flag = false;
 
     for (int index = 0; index < exponent; index++)
 	result *= base;
@@ -14,14 +40,41 @@ double power(double base, int exponent){
 int main(int argc, char *argv[]){
 
     double base, exponent;
+    int c;
+    int language = ENGLISH;
+    char user[MAX];
+    const char *user_language = NULL;
+    const char *sentence[] = {
+	"The result is: ",
+	"El reultado es: "
+    };
 
-    if (argc < 3)
-	return EXIT_FAILURE;
+    while ((c = getopt(argc, argv, "hl:")) != -1)
+	switch(c){
+	    case 'h':
+		print_usage(argv[0]);
+		break;
+	    case 'l':
+		user_language = optarg;
+		strncpy(user, user_language, MAX);
+		if(strcasecmp(user_language, "spanish") == 0)
+		    language = SPANISH;
+		break;
+	    case '?':
+		print_usage(argv[0]);
+		break;
+	}
 
-    base = atof(argv[1]);
-    exponent = atoi(argv[2]);
+    if (argc - optind < 2){
+	print_usage(argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
-    printf("%lf\n", power(base, exponent));
+    base = atof(argv[optind]);
+    exponent = atoi(argv[optind + 1]);
+
+    printf("%s%lf\n", sentence[language], power(base, exponent));
 
     return EXIT_SUCCESS;
 }
+
