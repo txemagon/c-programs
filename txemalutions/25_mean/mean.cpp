3187bdf08c2e29a3a1d0a995d5217a3dcc3f3510
txemagon <txema.gonz@gmail.com>
2014-04-04 12:46:36 +0200

    Struct example

---
+++ txemalutions/25_mean/mean.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N      3
+#define MAX 0x200
+
+struct TAlumno {
+    char nombre_completo[MAX];
+    double programacion;
+    double bases_de_datos;
+};
+
+int main(int argc, char *argv[]){
+
+    struct TAlumno clase[N];
+    
+    /* Pedir los datos */
+    for (int i=0; i<N; i++){
+        printf("Nombre completo: ");
+        gets( clase[i].nombre_completo );
+        printf("Nota de Programación: ");
+        scanf(" %lf", &clase[i].programacion );
+        printf("Nota de Bases de Datos: ");
+        scanf(" %lf", &clase[i].bases_de_datos );
+    }
+    
+
+    /* Imprimir los datos */
+    for (int i=0; i<N; i++){
+        printf("Nombre completo: %s\n", clase[i].nombre_completo );
+        printf("Nota de Programación: %.2lf\n", clase[i].programacion );
+        printf("Nota de Bases de Datos: %.2lf\n", clase[i].bases_de_datos );
+    }
+    return EXIT_SUCCESS;
+}
