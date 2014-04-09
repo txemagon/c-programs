727d2eb61f1f7e9de281210cfcfb015b6a7e4b64
txemagon <txema.gonz@gmail.com>
2014-04-09 10:22:43 +0200

    title with structs

--- txemalutions/25_mean/mean.cpp
+++ txemalutions/25_mean/mean.cpp
@@ -4,10 +4,14 @@
 #define N      3
 #define MAX 0x200
 
-struct TAlumno {
-    char nombre_completo[MAX];
+struct TContenidos {
     double programacion;
     double bases_de_datos;
+}
+
+struct TAlumno {
+    char nombre_completo[MAX];
+    struct TContenidos asignaturas;
 };
 
 int main(int argc, char *argv[]){
@@ -19,17 +23,17 @@ int main(int argc, char *argv[]){
         printf("Nombre completo: ");
         gets( clase[i].nombre_completo );
         printf("Nota de Programación: ");
-        scanf(" %lf", &clase[i].programacion );
+        scanf(" %lf", &clase[i].asignaturas.programacion );
         printf("Nota de Bases de Datos: ");
-        scanf(" %lf", &clase[i].bases_de_datos );
+        scanf(" %lf", &clase[i].asignaturas.bases_de_datos );
     }
     
 
     /* Imprimir los datos */
     for (int i=0; i<N; i++){
         printf("Nombre completo: %s\n", clase[i].nombre_completo );
-        printf("Nota de Programación: %.2lf\n", clase[i].programacion );
-        printf("Nota de Bases de Datos: %.2lf\n", clase[i].bases_de_datos );
+        printf("Nota de Programación: %.2lf\n", clase[i].asignaturas.programacion );
+        printf("Nota de Bases de Datos: %.2lf\n", clase[i].asignaturas.bases_de_datos );
     }
     return EXIT_SUCCESS;
 }
