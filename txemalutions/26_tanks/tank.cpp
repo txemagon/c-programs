c485d5ec3337b38fa497c12c346e152462deeec5
txemagon <txema.gonz@gmail.com>
2014-04-25 12:52:13 +0200

    tank v1.1

--- txemalutions/26_tanks/tank.cpp
+++ txemalutions/26_tanks/tank.cpp
@@ -1,34 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ncurses.h>
+
+#define DELTA_T 0.1
 
 struct TCoordenada2D{
    double x;
    double y;
 };
 
+struct TMovil{
+    struct TCoordenada2D posicion;
+    struct TCoordenada2D velocidad;
+    struct TCoordenada2D aceleracion;
+};
 
-void muestra(struct TCoordenada2D datos){
-    printf("(%.2lf, %.2lf)\n", datos.x, datos.y);
+void muestra(struct TMovil datos){
+    printf("Pos: (%.2lf, %.2lf)\t", datos.posicion.x, datos.posicion.y);
+    printf("Vel: (%.2lf, %.2lf)\t", datos.velocidad.x, datos.velocidad.y);
+    printf("Ace: (%.2lf, %.2lf)\n", datos.aceleracion.x, datos.aceleracion.y);
+}
+
+void pintar(struct TMovil movil){
+   mvprintw(20 - movil.posicion.y, movil.posicion.x, "O");
+   refresh();
 }
 
 int main(int argc, char *argv[]){
 
-    struct TCoordenada2D posicion = { 10, 0}, 
-                         velocidad, 
-                         aceleracion = {0, -10};
+    struct TMovil proyectil = {
+       {10, 0},
+       {0, 0},
+       {0, -10}
+    };
 
     /* Preguntar la velocidad inicialmente */
    printf("Velocidad Inicial (vx,vy): ");
-   scanf(" (%lf,%lf)", &velocidad.x, &velocidad.y);
+   scanf(" %lf,%lf", 
+         &proyectil.velocidad.x, 
+         &proyectil.velocidad.y);
+
+   initscr();
+   clear();
 
    /* Cada segundo saber lo que ha variado la velocidad y la posicion */
-   while(posicion.y >= 0){
-       velocidad.x += aceleracion.x;
-       velocidad.y += aceleracion.y;
-       posicion.x += velocidad.x;
-       posicion.y += velocidad.y;
-       muestra(posicion);
+   while(proyectil.posicion.y >= 0){
+       pintar(proyectil);
+
+       proyectil.velocidad.x += proyectil.aceleracion.x * DELTA_T;
+       proyectil.velocidad.y += proyectil.aceleracion.y * DELTA_T;
+       proyectil.posicion.x  += proyectil.velocidad.x   * DELTA_T;
+       proyectil.posicion.y  += proyectil.velocidad.y   * DELTA_T;
    }
+
+   getch();
+   endwin();
 
     return EXIT_SUCCESS;
 }
