af44db0f34d5e594de4dcd2aab97052f723ab55d
txemagon <txema.gonz@gmail.com>
2015-04-29 13:43:35 +0200

    bouncy

--- 2014/cpp/02_pelota/cpp/bouncy.cpp
+++ 2014/cpp/02_pelota/cpp/bouncy.cpp
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ncurses.h>
 #include <unistd.h>
 #include <time.h>
+#include <allegro5/allegro.h>
+
 #include "pelota.h"
 
 #define N 20
@@ -12,11 +13,23 @@ int
 main (int argc, char *argv[])
 {
 
+  ALLEGRO_DISPLAY *display = NULL;
+
   Pelota pelota[N];
   srand (time (NULL));
 
-  initscr ();
-  while (1)
+  if (!al_init()){
+      fprintf(stderr, "Me he quedado sin allegro");
+      return -1;
+  }
+
+  display = al_create_display(640, 480);
+  if (!display){
+      fprintf(stderr, "Me he quedado sin display.");
+      return -1;
+  }
+
+  while (0)
     {				/* Buzz Lightyear */
       /* Actualizar las coordenadas de la pelota */
       for (int i = 0; i < N; i++)
@@ -24,13 +37,17 @@ main (int argc, char *argv[])
 
       /* Pintar */
       for (int i = 0; i < N; i++)
-	mvprintw (pelota[i].get_y (), pelota[i].get_x (),
-		  "%c", pelota[i].get_dibujo ());
+        ; 
 
       usleep (100000);
-      refresh ();
     }
-  endwin ();
+
+  al_clear_to_color(al_map_rgb(0,66,0));
+  al_flip_display();
+
+  al_rest(10.0);
+
+  al_destroy_display(display);
 
   return EXIT_SUCCESS;
 }
