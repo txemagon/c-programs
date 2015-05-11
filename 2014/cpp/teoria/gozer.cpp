a6dfc02e975070326bde71290ddaa7e419492234
txemagon <txema.gonz@gmail.com>
2015-05-11 09:21:51 +0200

    destructor

---
+++ 2014/cpp/teoria/gozer.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+class Gozer
+{
+    public:
+
+	Gozer();
+	~Gozer();
+};
+
+Gozer::Gozer()
+{
+    cout << "Gozer the destructor has been created." << endl;
+}
+
+Gozer::~Gozer()
+{
+    cout << "Gozer the destructor has been destructed." << endl <<
+	"Now is more himself than ever" << endl;
+}
+
+int 
+main(int argc, char **argv)
+{
+    Gozer objeto;
+}
