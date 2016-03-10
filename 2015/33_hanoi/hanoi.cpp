2cd0a4fc8636a7761a899f19f738d6e098d12a70
txemagon <txema.gonz@gmail.com>
2016-03-10 14:31:35 +0100

    hanoi con fallo

--- 2015/33_hanoi/hanoi.cpp
+++ 2015/33_hanoi/hanoi.cpp
@@ -3,6 +3,7 @@
 #include <strings.h>
 
 #define MAX_PILA 10
+#define MAX_DISCO MAX_PILA
 #define PALOS 3
 
 struct Pila {
@@ -16,8 +17,14 @@ void inicializar(struct Pila *pila){
 
 bool push(struct Pila *pila, int dato){
 
+    /* Validación */
+    if (dato > MAX_PILA)
+        return false;
     if (pila->cima >= MAX_PILA)
         return false;
+    if (pila->cima > 0 && dato > pila->data[pila->cima-1])
+        return false;
+
     pila->data[pila->cima++] = dato;
     return true;
 }
@@ -26,18 +33,51 @@ int pop(struct Pila* pila){
     return pila->data[--pila->cima];
 }
 
+void pinta(int cuanto, char dibujo){
+    for (int i=0; i<cuanto; i++)
+        printf("%c", dibujo);
+
+}
+
 int main(int argc, const char **argv){
 
-    int destino, radio;
+    int destino, origen;
     struct Pila palo[PALOS];
 
     for (int p=0; p<PALOS; p++)
         inicializar(&palo[p]);
 
-    printf("palo, disco: ");
-    scanf(" %i, %i", &destino, &radio);
+    for (int disco=5; disco>0; disco--)
+        push(&palo[0], disco);
 
-    for (int p=0; p<PALOS; p++)
+    while(1){
+
+        system("clear");
+
+        for (int fila=MAX_PILA-1; fila>=0; fila--) {
+            for (int n_palo=0; n_palo<PALOS; n_palo++) {
+                printf("\t");
+                int asteriscos = palo[n_palo].data[fila];
+                int espacios   = MAX_DISCO - asteriscos;
+                if (fila >= palo[n_palo].cima){
+                    espacios = MAX_DISCO;
+                    asteriscos = 0;
+                }
+                pinta( espacios, ' ');
+                pinta( asteriscos, '*');
+                printf("|");
+                pinta( asteriscos, '*');
+                pinta( espacios, ' ');
+            }
+            printf("\n");
+        }
+
+        do {
+            printf("\norigen, destino: ");
+            scanf(" %i, %i", &origen, &destino);
+        } while(destino > PALOS || destino < 0 );
+        push( &palo[--destino], pop(&palo[--origen]));
+    }
 
     return EXIT_SUCCESS;
 }
