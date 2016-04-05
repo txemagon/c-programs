af78aedf5762d071356f745d54054281daf00332
txemagon <txema.gonz@gmail.com>
2016-04-05 11:34:03 +0200

    function pointers

---
+++ 2015/37_function_pointer/fp.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int suma(int op1, int op2) { return op1 + op2; }
+int resta(int op1, int op2) { return op1 - op2; }
+
+int main(int argc, const char **argv){
+
+    int (*p)(int, int);
+
+    p = suma;
+    int resultado = p(2,3);
+    printf("%i\n", resultado);
+
+    p = resta;
+    resultado = p(2,3);
+    printf("%i\n", resultado);
+
+
+
+    return EXIT_SUCCESS;
+}
