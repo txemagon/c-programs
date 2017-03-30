ba0ccb79d164f15793820bc6fb3b197fde6a803b
txemagon <txema.gonz@gmail.com>
2017-03-30 13:34:07 +0200

    pila de naves

--- 2016/31_pila_naves/include/pila.h
+++ 2016/31_pila_naves/include/pila.h
@@ -1,27 +1,36 @@
 #ifndef __PILA_H__
 #define __PILA_H__
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #define MAX_PILA 0x200
+#define MAX_X 100
+#define MAX_Y 100
+
 
 struct TMovil{
-    int x;
-    int y;
+    double x;
+    double y;
 };
 
 /* En esta lista el elemento sólo está activo mientras
  * está en la lista. El pop se cepilla al elemento. */
 
 struct TPila{
-    struct TMovil *data[MAX_PILA];
     int cima;
+    struct TMovil *data[MAX_PILA];
 };
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-    bool push(struct Pila *pila, struct TMovil *movil);
-    bool pop(struct Pila *pila);
+    bool push(struct TPila *pila, struct TMovil *movil);
+    bool pop(struct TPila *pila);
+    struct TMovil * crear_nave_aleatoria();
+    void mostrar(struct TMovil *dato);
+    void ver(struct TPila pila);
 
 #ifdef __cplusplus
 }
--- 2016/31_pila_naves/src/main.cpp
+++ 2016/31_pila_naves/src/main.cpp
@@ -1,7 +1,25 @@
-#include "general.h"
+#include <stdlib.h>
+#include <time.h>
+#include <strings.h>
+#include "pila.h"
+
+#define QNAVES 100
 
 int
-main()
+main ()
 {
-    return 0;
+    struct TPila objetos_pantalla;
+
+    srand(time(NULL));
+    bzero(&objetos_pantalla, sizeof(objetos_pantalla));
+
+    for (int i=0; i<QNAVES; i++)
+        push(&objetos_pantalla, crear_nave_aleatoria());
+
+
+    ver(objetos_pantalla);
+
+    while(pop(&objetos_pantalla));
+
+  return 0;
 }
