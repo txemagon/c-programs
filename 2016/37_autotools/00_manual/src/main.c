a8bdd79b350d9f63142155317d90824cd5896ad7
txemagon <txema.gonz@gmail.com>
2017-04-06 14:47:17 +0200

    autotools

---
+++ 2016/37_autotools/00_manual/src/main.c
@@ -0,0 +1,11 @@
+#include <config.h>
+#include <stdio.h>
+#include <stdtlib.h>
+
+int
+main (int argc, const char *argv[])
+{
+    printf("The World is a Vampire!\n");
+
+    return EXIT_SUCCESS;
+}
