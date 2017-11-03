d296e7f3f8c5724f477a851ac2e34b7b34f26e2d
txemagon <txema.gonz@gmail.com>
2017-11-03 14:32:27 +0100

    js hello

---
+++ 2017/00_primitivas/cpp/nombre.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int
+main()
+{
+    cout << "The World is a Vampire!" << endl;
+
+    return 0;
+}
---
+++ 2017/00_primitivas/javascript/javascript/main.js
@@ -0,0 +1,3 @@
+function main() {
+    alert("The World is a Vampire.")
+}
