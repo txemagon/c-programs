e33efc72cbe8b7ab780de8e441cb87651e99facd
txemagon <joseluisdamdosa@gmail.com>
2018-03-06 10:59:04 +0100

    life a punto

--- 2017/14_game_life/life.cpp
+++ 2017/14_game_life/life.cpp
@@ -4,18 +4,49 @@
 #include "global.h"
 #include "interfaz.h"
 
+#define MIN_SUPERV 2
+#define ASENTAR    3
+
+enum {actual, futuro, TIEMPOS};
+
+int vecinos(int f, int c, int matriz[M][N]){
+    int n_vecinos = 0;
+    for (int fv = f-1; fv < f + 2; fv++)
+        for (int cv=c-1; cv<c+2; cv++)
+            if (SON_VALIDAS(fv, cv))
+                n_vecinos += matriz[fv][cv];
+    n_vecinos--;
+    return n_vecinos;
+}
+
+void calcular(int futuro[M][N], int actual[M][N])
+{
+    int n_vecinos;
+
+    for (int f=0; f<M; f++)
+        for (int c=0; c<N; c++){
+            n_vecinos = vecinos(f, c, actual);
+            if (n_vecinos < MIN_SUPERV)
+                futuro[f][c] = 0;
+            if (n_vecinos ==  ASENTAR)
+                futuro[f][c] = 1;
+            if (n_vecinos >  ASENTAR)
+                futuro[f][c] = 0;
+        }
+
+}
+
 int main(){
 
-    int actual[M][N],
-        futuro[M][N];
+    int mundo[TIEMPOS][M][N];
 
-    bzero(actual, sizeof(actual));
-    poblacion_inicial(actual);
+    bzero(mundo[actual], sizeof(mundo[actual]));
+    poblacion_inicial(mundo[actual]);
 
     while(1) {
-        // calcular(futuro, actual);
-        // Convertir el futuro en actual
-        pintar(actual);
+        system("clear");
+        calcular(mundo[futuro], mundo[actual]);
+        pintar(mundo[actual]);
     }
 
     return EXIT_SUCCESS;
