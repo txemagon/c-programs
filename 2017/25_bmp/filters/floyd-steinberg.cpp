6286ef7984429e87658f0ee8f7c73f2574f36603
txemagon <joseluisdamdosa@gmail.com>
2018-05-25 14:48:15 +0200

    ejemplo de filtro completo

--- 2017/25_bmp/filters/floyd-steinberg.cpp
+++ 2017/25_bmp/filters/floyd-steinberg.cpp
@@ -3,6 +3,8 @@
 #include <math.h>
 #include "bmp.h"
 
+#define R3 1.7321
+
 const char *source= "a.bmp";
 const char *destin= "b.bmp";
 
@@ -24,7 +26,7 @@ Color *leer_cabecera(Header *header, const char *filename) {
 
     fseek(pbmp, header->bmp.data_offset, SEEK_SET);
     imagen = (Color *) malloc(header->dib.image_size);
-    fread(imagen, sizeof(Color), header->dib.image_size / header->dib.bpp, pbmp);
+    fread(imagen, sizeof(Color), 8 * header->dib.image_size / header->dib.bpp, pbmp);
     fclose(pbmp);
 
     return imagen;
@@ -47,21 +49,37 @@ unsigned char *leer_archivo(const char *filename, Header header){
 }
 
 int main(){
-    unsigned char *bmp;  /* All the file */
+    unsigned char *bmp          = NULL;  /* All the file */
+    unsigned char *luminosidad  = NULL;
     Color *imagen;       /* Image data */
     Header header;
+    int pixels = 0;
 
     imagen = leer_cabecera(&header, source);
     bmp = leer_archivo(source, header);
 
-    /* Escala de grises
-    for (int i=0; i<pixels; i++){
-        unsigned char media = (unsigned char) sqrt( (imagen[i].r * imagen[i].r + imagen[i].g * imagen[i].g + imagen[i].b * imagen[i].b) ) ;
-        imagen[i].r = imagen[i].g = imagen[i].b =  (media > 100) ? (unsigned char) 255 : 0;
-    }
-*/
+    /* Calcular la luminosidad de la imagen */
+    pixels = 8 * header.dib.image_size / header.dib.bpp;
+    fprintf(stderr, "pixels: %i\n",  pixels);
+    luminosidad = (unsigned char *) malloc(pixels);
+    for (int i=0; i<pixels; i++)
+        *(luminosidad + i) = (unsigned char) (sqrt(
+                imagen[i].r * imagen[i].r +
+                imagen[i].g * imagen[i].g +
+                imagen[i].b * imagen[i].b ) / R3 );
 
+
+
+    /* Filtro */
+    for (unsigned i=0; i<header.dib.image_size; i++)
+       *(bmp + header.bmp.data_offset + i ) = *(luminosidad + i / 3);
+    /* Fin del Filtro */
+
+
+    /* Salida */
     fwrite(bmp, 1, header.bmp.size, stdout);
+
+    free(luminosidad);
     free(bmp);
     free(imagen);
 
